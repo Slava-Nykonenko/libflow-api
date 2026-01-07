ec28d3da04bc096fc7a5cc054cc6d3b76b1af5e5
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-01-07 21:15:46 +0000

    Borrowings (feat): configure API routing

---
+++ borrowings/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from borrowings.views import BorrowingViewSet
+
+app_name = "borrowings_app"
+
+router = DefaultRouter()
+router.register("", BorrowingViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
