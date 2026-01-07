e3c62250cb9bc3db66ce98e4b97654ac2e42bb61
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-01-07 21:09:23 +0000

    Borrowings (feat): implement BorrowingViewSet

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.db import transaction
+from rest_framework.viewsets import ModelViewSet
+
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingRetrieveSerializer,
+)
+
 
 # Create your views here.
+class BorrowingViewSet(ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        elif self.action == "retrieve":
+            return BorrowingRetrieveSerializer
+        return BorrowingSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset.select_related("user", "book")
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        if self.action in ("list", "retrieve"):
+            user_id = self.request.query_params.get("user_id")
+            is_active = self.request.query_params.get("is_active")
+            if user_id:
+                queryset = queryset.filter(user=user_id)
+            if is_active:
+                queryset = queryset.filter(
+                    actual_return_date__isnull=is_active.lower() == "true"
+                )
+        return queryset
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            borrowing = serializer.save(user=self.request.user)
+            book = borrowing.book
+            book.inventory -= 1
+            book.save()
