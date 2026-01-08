39b6030273b175b3e21269ef260308d2bd4239a2
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-01-08 17:24:00 +0000

    LibFlow (feat): implement Telegram notifications (borrowing creation and book returning)

---
+++ borrowings/notifications.py
@@ -0,0 +1,16 @@
+import requests
+from django.conf import settings
+
+
+def send_telegram_notification(message):
+    bot_token = settings.TELEGRAM_BOT_TOKEN
+    chat_id = settings.TELEGRAM_CHAT_ID
+
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": message, "parse_mode": "HTML"}
+
+    try:
+        response = requests.post(url, data=payload)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Telegram error: {e}")
--- borrowings/views.py
+++ borrowings/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from borrowings.models import Borrowing
+from borrowings.notifications import send_telegram_notification
 from borrowings.serializers import (
     BorrowingSerializer,
     BorrowingListSerializer,
@@ -47,6 +48,14 @@ class BorrowingViewSet(ModelViewSet):
             book.inventory -= 1
             book.save()
 
+            message = (
+                f"<b>A new borrowing!</b>\n"
+                f"User: {borrowing.user.email}\n"
+                f"Book: {book.title}\n"
+                f"Expected return date: {borrowing.expected_return_date}"
+            )
+            send_telegram_notification(message)
+
     @action(detail=True, methods=["post"], url_path="return")
     def book_return(self, request, pk=None):
         borrowing = self.get_object()
@@ -62,6 +71,13 @@ class BorrowingViewSet(ModelViewSet):
             book = borrowing.book
             book.inventory += 1
             book.save()
+
+            message = (
+                f"<b>A book returned!</b>\n"
+                f"User: {borrowing.user.email}\n"
+                f"Book: {book.title}"
+            )
+            send_telegram_notification(message)
 
         return Response(
             {"detail": "The book returned successfully."}, status=status.HTTP_200_OK
